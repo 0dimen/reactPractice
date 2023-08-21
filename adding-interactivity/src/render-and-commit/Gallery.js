0febf53f725ec026938637b39b916d59884149d1
0dimen <first11@konkuk.ac.kr>
2023-08-21 23:25:27 +0900

    render and commit

---
+++ adding-interactivity/src/render-and-commit/Gallery.js
@@ -0,0 +1,13 @@
+import Image from "./Image";
+
+
+export default function Gallery(){
+    return(
+        <section>
+            <h1>Inspiring Sculptures</h1>
+            <Image />
+            <Image />
+            <Image />
+        </section>
+    );
+}
